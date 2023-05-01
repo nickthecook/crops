39eb0a40ffe1cce240ae4c45cbf56f1553a72e5a
nickthecook <nickthecook@gmail.com>
2023-05-01 17:19:48 -0400

    move build stuff to build/

--- bin/build-platforms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-containers="crystallang/crystal:latest-alpine"
-
-for container in $containers; do
-  time docker run --rm -it -v $(pwd):/workspace -w /workspace "$container" bin/build.sh
-done
