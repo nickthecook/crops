b1089c5b0cde19cdd64bcd93207addd26c500a8b
nickthecook <nickthecook@gmail.com>
2023-05-08 17:42:05 -0400

    release 2.0.0.rc16

--- ops_team.gemspec
+++ ops_team.gemspec
@@ -2,7 +2,7 @@
 
 Gem::Specification.new do |s|
 	s.name = 'ops_team'
-	s.version = '2.0.0.rc15'
+	s.version = '2.0.0.rc16'
 	s.authors = [
 		'nickthecook@gmail.com'
 	]
--- src/version.cr
+++ src/version.cr
@@ -2,7 +2,7 @@ require "semantic_version"
 
 class Version
   def self.name_and_version
-    "crops-#{version}.rc15"
+    "crops-#{version}.rc16"
   end
 
   def self.version : String
