bd1f529d1f94acefd59dec1ccbba691bf551eb93
nickthecook <nickthecook@gmail.com>
2023-05-08 17:41:23 -0400

    return non-zero if ops.yml missing for some action

---
+++ spec/e2e/ops_yml_does_not_exist/e2e_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+RSpec.describe "no actions" do
+	include_context "ops e2e"
+
+	before(:all) do
+		Dir.chdir(__dir__)
+
+		remove_untracked_files
+	end
+
+	let(:success_commands) do
+		[
+			"../../../build/ops version",
+			"../../../build/ops init",
+			"../../../build/ops help"
+		]
+	end
+	let(:error_commands) do
+		[
+			"../../../build/ops up",
+			"../../../build/ops down",
+			"../../../build/ops test"
+		]
+	end
+	let(:success_results) { success_commands.map { |cmd| run_ops(cmd) } }
+	let(:error_results) { error_commands.map { |cmd| run_ops(cmd) } }
+	let(:success_exit_codes) { success_results.map { |result| result[EXIT_CODE_IDX] } }
+	let(:error_exit_codes) { error_results.map { |result| result[EXIT_CODE_IDX] } }
+
+	it "succeeds when action does not require ops.yml" do
+		expect(success_exit_codes).to all(eq(0))
+	end
+
+	it "fails when action does require ops.yml" do
+		expect(error_exit_codes).to all(eq(72))
+	end
+end
