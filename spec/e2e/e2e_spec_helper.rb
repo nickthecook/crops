db41643a55679400a501c5ad288e270181b6b935
nick <nicholas.newberry@rcgtconsulting.com>
2023-05-09 11:04:16 -0400

    support user template dir

--- spec/e2e/e2e_spec_helper.rb
+++ spec/e2e/e2e_spec_helper.rb
@@ -10,13 +10,15 @@ shared_context "ops e2e" do
 	ENV["OPS_RUNNING"] = nil
 	ENV["OPS_DEBUG_OUTPUT"] = "true"
 
+	let(:ops_env_vars) { {} }
+
 	def remove_untracked_files
 		untracked_files = `git ls-files --others | grep -v '.rb$' | grep -v '$.yml'`.split("\n")
 		untracked_files.each { |file| `rm -f #{file}` }
 	end
 
 	def run_ops(cmd, output_file = "ops.out")
-		output, status = Open3.capture2e(cmd)
+		output, status = Open3.capture2e(ops_env_vars, cmd)
 
 		File.write(output_file, output)
 
