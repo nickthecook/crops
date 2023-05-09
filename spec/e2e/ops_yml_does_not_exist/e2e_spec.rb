5d1313cfde9b6a2ac98bb6cb4e310836b6d596db
nickthecook <nickthecook@gmail.com>
2023-05-09 09:27:10 -0400

    test missing ops.yml error output

--- spec/e2e/ops_yml_does_not_exist/e2e_spec.rb
+++ spec/e2e/ops_yml_does_not_exist/e2e_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe "no actions" do
 	let(:error_results) { error_commands.map { |cmd| run_ops(cmd) } }
 	let(:success_exit_codes) { success_results.map { |result| result[EXIT_CODE_IDX] } }
 	let(:error_exit_codes) { error_results.map { |result| result[EXIT_CODE_IDX] } }
+	let(:error_outputs) { error_results.map { |result| result[OUTPUT_IDX] } }
 
 	it "succeeds when action does not require ops.yml" do
 		expect(success_exit_codes).to all(eq(0))
@@ -38,5 +39,9 @@ RSpec.describe "no actions" do
 
 	it "fails when action does require ops.yml" do
 		expect(error_exit_codes).to all(eq(72))
+	end
+
+	it "outputs an error message when it fails" do
+		expect(error_outputs).to all match(/File 'ops.yml' does not exist./)
 	end
 end
