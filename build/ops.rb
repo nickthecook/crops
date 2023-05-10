1444d2ac92fa865854b6e667ea0e840517fb8feb
nickthecook <nickthecook@gmail.com>
2023-05-10 14:49:15 -0400

    add brew formula

---
+++ build/ops.rb
@@ -0,0 +1,25 @@
+# put this in /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/ops.rb
+class Ops < Formula
+  desc "Lightweight automation for your project"
+  homepage "https://github.com/nickthecook/crops"
+  version "2.0.0"
+  url "https://github.com/nickthecook/crops/archive/refs/tags/#{version}.tar.gz"
+  sha256 "56761ee62dfbaca23312fa3abfff776a7e8d86f61eb8aac3a493517cf8a00ff1"
+  license "GPL-3.0-only"
+
+  depends_on "crystal" => :build
+  depends_on "bdw-gc"
+  depends_on "libevent"
+  depends_on "libyaml"
+  depends_on "pcre2"
+  
+  def install
+    local_crystal_path = `crystal env CRYSTAL_PATH`.chomp
+    system("CRYSTAL_PATH='src:lib:#{local_crystal_path}' crystal build -o ops ops.cr")
+    bin.install "ops"
+  end
+
+  test do
+    system "#{bin}/ops", "version"
+  end
+end
